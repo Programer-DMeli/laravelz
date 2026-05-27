765f2a56df9db76c69937d9deae79f87e91fe285
Programer-DMeli <carbajalmeli369@gmail.com>
2026-05-27 10:44:35 -0500

    primer commit

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,7 @@ Route::post('/suma', function (Request $request) {
 
 Route::get('productos', [ProductoController::class, 'index']);
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
